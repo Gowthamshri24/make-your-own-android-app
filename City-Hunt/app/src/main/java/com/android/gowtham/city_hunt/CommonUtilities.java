229458f3b40b3ae635cde5ab2dd73fdbc57c6383
Gowthamshri24 <mdgowthamshri@gmail.com>
2015-04-02 06:57:37 +0530

    Committed by Udacity

---
+++ City-Hunt/app/src/main/java/com/android/gowtham/city_hunt/CommonUtilities.java
@@ -0,0 +1,20 @@
+package com.android.gowtham.city_hunt;
+
+public final class CommonUtilities {
+	
+	// give your server registration url here
+    static final String SERVER_URL = "http://honeykumar.byethost17.com/gcm_server_php/register.php";
+
+    // Google project id
+    static final String SENDER_ID = "943400181377";
+
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "AndroidHive GCM";
+
+    static final String DISPLAY_MESSAGE_ACTION =
+            "com.android.gowtham.city_hunt.DISPLAY_MESSAGE";
+
+    static final String EXTRA_MESSAGE = "message";
+}
